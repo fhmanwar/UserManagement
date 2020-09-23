2930c204b0ade853ace6566af363be1e0beae4fe
fhmanwar <fhmianwar@gmail.com>
2020-09-23 14:50:11 +0700

    Initial Commit Client

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using API.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("http://winarto-001-site1.dtempurl.com/api/")
+        };
+
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/login");
+        }
+
+        [Route("verify")]
+        public IActionResult Verify()
+        {
+            return View();
+        }
+
+        [Route("notfound")]
+        public IActionResult Notfound()
+        {
+            return View();
+        }
+
+        [Route("validate")]
+        public IActionResult Validate(UserVM userVM)
+        {
+            var json = JsonConvert.SerializeObject(userVM);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            if (userVM.Name == null)
+            {
+                HttpResponseMessage result = null;
+                if (userVM.VerifyCode != null)
+                { // Verify Code
+                    result = client.PostAsync("auths/code/", byteContent).Result;
+                    SendLogs(userVM.Email + " VerifyCode Successfully", userVM.Email);
+                }
+                else if (userVM.VerifyCode == null)
+                { // Login
+                    result = client.PostAsync("auths/login/", byteContent).Result;
+                }
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var data = result.Content.ReadAsStringAsync().Result;
+                    if (data != null)
+                    {
+                        HttpContext.Session.SetString("token", "Bearer " + data);
+                        var handler = new JwtSecurityTokenHandler();
+                        var tokenS = handler.ReadJwtToken(data);
+                        var jwtPayloadSer = JsonConvert.SerializeObject(tokenS.Payload.ToDictionary(x => x.Key, x => x.Value));
+                        var jwtPayloadDes = JsonConvert.DeserializeObject(jwtPayloadSer).ToString();
+                        var account = JsonConvert.DeserializeObject<UserVM>(jwtPayloadSer);
+
+                        if (!account.VerifyCode.Equals(""))
+                        {
+                            return Json(new { status = true, msg = "VerifyCode" });
+                        }
+                        else if (account.RoleName != null)
+                        {
+                            HttpContext.Session.SetString("id", account.Id);
+                            //HttpContext.Session.SetString("name", account.Name);
+                            //HttpContext.Session.SetString("email", account.Email);
+                            HttpContext.Session.SetString("lvl", account.RoleName);
+                            SendLogs(userVM.Email + " Login", userVM.Email);
+                            if (account.RoleName == "Admin")
+                            {
+                                return Json(new { status = true, msg = "Login Successfully !" });
+                            }
+                            return Json(new { status = true, msg = "Login Successfully !" });
+                        }
+                        return Json(new { status = false, msg = "You Don't Have Permissions! Please Contact Administrator" });
+                    }
+                    return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+                }
+                return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+            }
+            else if (userVM.Name != null)
+            { // Register
+                var result = client.PostAsync("auths/register/", byteContent).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    SendLogs(userVM.Email + " Register", userVM.Email);
+                    return Json(new { status = true, code = result, msg = "Register Success! " });
+                }
+                return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+            }
+            return Redirect("/login");
+        }
+
+        public IActionResult SendLogs(string response, string msg)
+        {
+            var log = new LogVM
+            {
+                Response = response,
+                Email = msg,
+            };
+            var jsonLog = JsonConvert.SerializeObject(log);
+            var bufferLog = System.Text.Encoding.UTF8.GetBytes(jsonLog);
+            var byteContentLog = new ByteArrayContent(bufferLog);
+            byteContentLog.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var sendLog = client.PostAsync("logs/", byteContentLog).Result;
+            return Json(sendLog);
+        }
+
+    }
+}
---
+++ Web/wwwroot/js/script/AuthScript.js
@@ -0,0 +1,118 @@
+﻿var arr = [];
+function Login() {
+    debugger;
+    var validate = new Object();
+    validate.Email = $('#Email').val();
+    validate.Password = $('#Password').val();
+    $.ajax({
+        type: 'POST',
+        url: "/validate/",
+        cache: false,
+        dataType: "JSON",
+        data: validate
+    }).then((result) => {
+        debugger;
+        if (result.status == true) {
+            if (result.msg == "VerifyCode") {
+                $.notify({
+                    // options
+                    icon: 'flaticon-alarm-1',
+                    title: 'Notification',
+                    message: 'Login Successfully',
+                }, {
+                    // settings
+                    element: 'body',
+                    type: "success",
+                    allow_dismiss: true,
+                    placement: {
+                        from: "top",
+                        align: "center"
+                    },
+                    timer: 1500,
+                    delay: 5000,
+                    animate: {
+                        enter: 'animated fadeInDown',
+                        exit: 'animated fadeOutUp'
+                    },
+                });
+                window.setTimeout(function () {
+                    location.reload();
+                    window.location.href = "/verify?mail=" + validate.Email;
+                }, 2000);
+            } else {
+                $.notify({
+                    // options
+                    icon: 'flaticon-alarm-1',
+                    title: 'Notification',
+                    message: 'Login Successfully',
+                }, {
+                    // settings
+                    element: 'body',
+                    type: "success",
+                    allow_dismiss: true,
+                    placement: {
+                        from: "top",
+                        align: "center"
+                    },
+                    timer: 1500,
+                    delay: 5000,
+                    animate: {
+                        enter: 'animated fadeInDown',
+                        exit: 'animated fadeOutUp'
+                    },
+                    icon_type: 'class',
+                    template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                        '<span data-notify="icon"></span> ' +
+                        '<span data-notify="title">{1}</span> ' +
+                        '<span data-notify="message">{2}</span>' +
+                        '<div class="progress" data-notify="progressbar">' +
+                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                        '</div>' +
+                        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                        '</div>'
+                });
+                window.setTimeout(function () {
+                    location.reload();
+                    window.location.href = "/";
+                }, 2000);
+            }
+        } else {
+            $.notify({
+                // options
+                icon: 'flaticon-alarm-1',
+                title: 'Notification',
+                message: result.msg,
+            }, {
+                // settings
+                element: 'body',
+                type: "danger",
+                allow_dismiss: true,
+                placement: {
+                    from: "top",
+                    align: "center"
+                },
+                timer: 1000,
+                delay: 5000,
+                animate: {
+                    enter: 'animated fadeInDown',
+                    exit: 'animated fadeOutUp'
+                },
+                icon_type: 'class',
+                template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                    '<span data-notify="icon"></span> ' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<div class="progress" data-notify="progressbar">' +
+                    '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                    '</div>' +
+                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                    '</div>'
+            });
+            window.setTimeout(function () {
+                location.reload();
+            }, 2000);
+        }
+    })
+}
